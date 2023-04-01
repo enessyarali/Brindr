73e9b7501b85994b25ea7aa38366015ba074ee3a
Enes Yarali <s302845@studenti.polito.it>
2023-04-01 15:37:25 +0200

    Page Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import OnBoarding from "./pages/OnBoarding";
+import Dashboard from "./pages/DashBoard";
+import {BrowserRouter , Routes , Route } from 'react-router-dom'
 
-function App() {
+
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+    <Routes>
+        <Route path={"/"} element={<Home/>}/>
+        <Route path={"/dashboard"} element={<Dashboard/>}/>
+        <Route path={"/onboarding"} element={<OnBoarding/>}/>
+        <Route />
+    </Routes>
+
+    </BrowserRouter>
+   
   );
 }
 
---
+++ client/src/pages/OnBoarding.js
@@ -0,0 +1,9 @@
+const OnBoarding = () => {
+    return(
+        <div>
+                Onboarding
+        </div>
+    )
+}
+
+export default OnBoarding
